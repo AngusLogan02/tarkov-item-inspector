b2e981f90bf9f464442bc1b03463a9397fc3cea4
Angus Logan <angus.logan@baronsview.com>
2021-12-22 21:08:56 +0000

    hotkey implemented, framework in place for doing flea market in the future

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import mss
 import numpy as np
-def getCells():
+def getCellsTrader():
     # each cell is a 64x64 pixel RGB image
     top = 261
     left = 8
--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import stateModel
 import image
 import mouse
 import time
+import keyboard
 
-# stateModel.train(550, 200)
-
+#stateModel.train(550, 200)
 model = stateModel.loadModel()
-cell, coord = image.getCells()
-for i in range(0, len(cell)):
-    prediction = model.predict(cell[i])
-    score = prediction[0]
-    if score > 0.7:
-        print("cell " + str(i) + " UNINSPECTED (" + str(int(score*100)) + "% certain)")
-        mouse.middleMouse(coord[i][0], coord[i][1])
-        time.sleep(0.6)
-    else:
-        print("cell " + str(i) + " already inspected (" + str(int((1-score)*100)) + "% certain)")
+
+def trader():
+    cell, coord = image.getCellsTrader()
+    for i in range(0, len(cell)):
+        prediction = model.predict(cell[i])
+        score = prediction[0]
+        if score > 0.7:
+            print("cell " + str(i) + " UNINSPECTED (" + str(int(score*100)) + "% certain)")
+            mouse.middleMouse(coord[i][0], coord[i][1])
+            time.sleep(0.6)
+        else:
+            print("cell " + str(i) + " already inspected (" + str(int((1-score)*100)) + "% certain)")
+
+keyboard.add_hotkey("shift+t", lambda: trader())
+keyboard.wait("shift+q")
