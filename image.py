ece6d7936dd773826f0ea9dcaecb8a6c24d3fd6f
Angus Logan <angus.logan@baronsview.com>
2021-12-22 15:55:13 +0000

    all cells now get ran through the model, refactored image to use PIL instead of OpenCV as PIL plays nicer with Keras

--- image.py
+++ image.py
@@ -1,29 +1,20 @@
-import cv2
+from PIL import Image
 import mss
 import numpy as np
-#img = cv2.imread("data/raw/6.png")
-def getCells():
+def getCellsPIL():
+    top = 261
+    left = 8
+    size = 64
+    bruh = 0
+    cell = []
     with mss.mss() as sct:
-        img = np.array(sct.grab(sct.monitors[1]))
-        img = np.flip(img[:, :, :3], 2)  # 1
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 2
-        x1 = 8
-        x2 = 72
-        y1 = 261
-        y2 = 325
-        cell = []
-
-        for i in range(0, 11):
+        for i in range(0,11):
             for j in range(0,10):
-                imgCropped = img[y1:y2, x1:x2].copy()
-                #cv2.imwrite("cell" + str(i) + str(j) + ".png", imgCropped)
-                cell.append(imgCropped)
-                x1 += 63
-                x2 += 63
-            x1 = 8
-            x2 = 72
-            y1 += 63
-            y2 += 63
-    cell = np.array(cell)
-    cell = cell[None, :]
+                sct_img = sct.grab({"top": top, "left": left, "width": size, "height": size})
+                pilImage = np.array(Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX"))
+                cell.append(pilImage[None, :])
+                bruh+=1
+                left+=63
+            left = 8
+            top+=63
     return cell
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import stateModel
 import image
-import cv2
 
 #stateModel.train(550, 20)
 
 model = stateModel.loadModel()
 cell = image.getCells()
-test = cell[0]
-prediction = model.predict(test)
-score = prediction[0]
-if score > 0.7:
-    print("UNINSPECTED (" + str(int(score*100)) + "% certain)")
-else:
-    print("already inspected (" + str(int((1-score)*100)) + "% certain)")
+for i in range(0, len(cell)):
+    prediction = model.predict(cell[i])
+    score = prediction[0]
+    if score > 0.7:
+        print("UNINSPECTED (" + str(int(score*100)) + "% certain)")
+    else:
+        print("already inspected (" + str(int((1-score)*100)) + "% certain)")
