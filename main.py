6f36f54c7a55d8a391edd973a2b57ddea793e428
Angus Logan <angus.logan@baronsview.com>
2022-06-17 12:40:06 +0100

    fleshed out welcome message

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import time
 import keyboard
 import os
 
-#stateModel.train(550, 200)
-print("Loading...")
-model = stateModel.loadModel()
-os.system("cls")
-print("+----------------------------------------------------------------------------------------------+")
-print("| Press 'Shift + T' to inspect from a trader's inventory, and 'Shift + Q' to quit the program. |")
-print("| Ready to inspect.                                                                            |")
-print("+--------------------------------------------------------------------------------------------- +")
-print()
+def welcomeMessage():
+    os.system("cls")
+    print("+----------------------------------------------------------------------------------------------+")
+    print("| NOTE: Tarkov Item Inspector currently only works in 1920x1080p. If your monitor's native     |")
+    print("| resolution is different from this, make sure the game is running in 1920x1080p FULLSCREEN    |")
+    print("| mode.                                                                                        |")
+    print("|                                                                                              |")
+    print("| Press 'Shift + T' to inspect from a trader's inventory, and 'Shift + Q' to quit the program. |")
+    print("| Ready to inspect.                                                                            |")
+    print("+----------------------------------------------------------------------------------------------+")
+    print()
 
 def trader():
     print("STARTING TRADER INSPECTION")
@@ -27,6 +29,14 @@ def trader():
             time.sleep(0.6)
         else:
             print("cell " + str(i) + " already inspected (" + str(int((1-score)*100)) + "% certain)")
+
+    welcomeMessage()
+
+#stateModel.train(550, 200)
+print("Loading...")
+model = stateModel.loadModel()
+
+welcomeMessage()
 
 keyboard.add_hotkey("shift+t", lambda: trader())
 keyboard.wait("shift+q")
